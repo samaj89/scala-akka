7df959ca9617dce0d941484a61495aa8fa66432a
samaj89 <samajansen89@gmail.com>
2017-11-12 13:21:07 +0000

    Implemented main method, greetings passed

--- src/main/scala/greetings/AkkaGreeters.scala
+++ src/main/scala/greetings/AkkaGreeters.scala
@@ -1,9 +1,9 @@
 package greetings
 
-import akka.actor.Actor.Receive
-import akka.actor.{Actor, ActorLogging, ActorRef, Props}
-import greetings.AkkaGreeters.Greeter.{Greet, WhoToGreet}
-import greetings.AkkaGreeters.Printer.Greeting
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
+import greetings.AkkaGreeters.Greeter._
+import greetings.AkkaGreeters.{Greeter, Printer}
+import greetings.AkkaGreeters.Printer._
 
 /**
   * A basic Akka example involving passing greetings
@@ -38,5 +38,36 @@ object AkkaGreeters {
     def props: Props = Props[Printer]
     final case class Greeting(greeting: String)
   }
+
+}
+
+object GreetersMain extends App {
+
+  val system: ActorSystem = ActorSystem("greetAkka")
+
+  val printer: ActorRef = system.actorOf(Printer.props)
+
+  val englishGreeter: ActorRef =
+    system.actorOf(Greeter.props("Good morning", printer), "englishGreeter")
+  val frenchGreeter: ActorRef =
+    system.actorOf(Greeter.props("Bonjour", printer), "frenchGreeter")
+  val germanGreeter: ActorRef =
+    system.actorOf(Greeter.props("Guten Morgen", printer), "germanGreeter")
+  val russianGreeter: ActorRef =
+    system.actorOf(Greeter.props("Dobroe utro", printer), "russianGreeter")
+
+  englishGreeter ! WhoToGreet("Mr Smith")
+  englishGreeter ! Greet
+
+  frenchGreeter ! WhoToGreet("mademoiselle")
+  frenchGreeter ! Greet
+
+  germanGreeter ! WhoToGreet("Frau Oebinger")
+  germanGreeter ! Greet
+
+  russianGreeter ! WhoToGreet("Alyosha")
+  russianGreeter ! Greet
+
+  system.terminate()
 
 }
