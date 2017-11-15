ebf51bdac16335062fd74c6e1b91be90ab898119
samaj89 <samajansen89@gmail.com>
2017-11-15 15:14:58 +0000

    Added Device actor class, renamed file

---
+++ src/main/scala/tutorials/IotApp.scala
@@ -0,0 +1,53 @@
+package tutorials
+
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import tutorials.Device.{ReadTemperature, RespondTemperature}
+
+import scala.io.StdIn
+
+/**
+  * Code baseed on the Akka tutorial at https://doc.akka.io/docs/akka/current/scala/guide/tutorial_2.html
+  */
+
+object IotSupervisor {
+  def props(): Props = Props(new IotSupervisor)
+}
+
+class IotSupervisor extends Actor with ActorLogging {
+  override def preStart(): Unit = log.info("IoT Application started")
+  override def postStop(): Unit = log.info("IoT Application stopped")
+
+  override def receive = Actor.emptyBehavior
+}
+
+object Device {
+  def props(groupId: String, deviceId: String): Props = Props(new Device(groupId, deviceId))
+
+  final case class ReadTemperature(requestId: Long)
+  final case class RespondTemperature(requestId: Long, value: Option[Double])
+}
+
+class Device(groupId: String, deviceId: String) extends Actor with ActorLogging {
+  var lastTemperatureReading: Option[Double] = None
+
+  override def preStart(): Unit = log.info("Device actor {}-{} started", groupId, deviceId)
+  override def postStop(): Unit = log.info("Device actor {}-{} stopped", groupId, deviceId)
+
+  override def receive = {
+    case ReadTemperature(id) =>
+      sender() ! RespondTemperature(id, lastTemperatureReading)
+  }
+}
+
+object IotApp {
+  def main(args: Array[String]): Unit = {
+    val system = ActorSystem("iot-system")
+
+    try {
+      val supervisor = system.actorOf(IotSupervisor.props(), "iot-supervisor")
+      StdIn.readLine()
+    } finally {
+      system.terminate()
+    }
+  }
+}
