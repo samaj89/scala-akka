19d4dd16b8fed4efa7022270b8fe02e50305a5a7
samaj89 <samajansen89@gmail.com>
2017-11-15 15:50:36 +0000

    Added write functionality to Device actor, wrote test for write-read functionality

--- src/test/scala/tutorials/DeviceSpec.scala
+++ src/test/scala/tutorials/DeviceSpec.scala
@@ -28,4 +28,25 @@ class DeviceSpec(_system: ActorSystem)
     response.requestId should ===(42)
     response.value should ===(None)
   }
+
+  "A Device actor" should "reply with latest temperature reading" in {
+    val probe = TestProbe()
+    val deviceActor = system.actorOf(Device.props("group", "device"))
+
+    deviceActor.tell(Device.RecordTemperature(requestId = 1, 24.0), probe.ref)
+    probe.expectMsg(Device.TemperatureRecorded(requestId = 1))
+
+    deviceActor.tell(Device.ReadTemperature(requestId = 2), probe.ref)
+    val response1 = probe.expectMsgType[Device.RespondTemperature]
+    response1.requestId should ===(2)
+    response1.value should ===(Some(24.0))
+
+    deviceActor.tell(Device.RecordTemperature(requestId = 3, 55.0), probe.ref)
+    probe.expectMsg(Device.TemperatureRecorded(requestId = 3))
+
+    deviceActor.tell(Device.ReadTemperature(requestId = 4), probe.ref)
+    val response2 = probe.expectMsgType[Device.RespondTemperature]
+    response2.requestId should ===(4)
+    response2.value should ===(Some(55.0))
+  }
 }
